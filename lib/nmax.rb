ed33e7b402cc6231b8e3ae543ae3a8087050bc2d
LKS007 <leon99_21@mail.ru>
2018-11-06 01:02:59 +0300

    Add work functional with bin directory

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,5 +1,16 @@
 require "nmax/version"
 
 module Nmax
-  # Your code goes here...
+  def self.get_sequences(input_text, number_of_sequences)
+    numbers_array = Array.new
+
+    input_text.each_line do |line|
+      numbers_in_line = line.gsub(/[0-9]{1,1000}/)
+      numbers_in_line = numbers_in_line.map(&:to_i)
+      numbers_array.concat(numbers_in_line)
+    end
+    
+    numbers_array.sort!.reverse!
+    numbers_array[0...number_of_sequences]
+  end
 end
