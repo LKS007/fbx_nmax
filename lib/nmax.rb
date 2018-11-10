0ea4bf482d8e390b511cfb64829572d491247248
LKS007 <leon99_21@mail.ru>
2018-11-11 02:54:28 +0300

    Add tests, fix functional

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -2,6 +2,9 @@ require "nmax/version"
 
 module Nmax
   def self.get_sequences(input_text, number_of_sequences)
+    if !number_of_sequences.is_a? (Integer) and number_of_sequences.to_i == 0
+      return "Wrong argument - should be integer" 
+    end
     numbers_array = Array.new
 
     input_text.each_line do |line|
--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
+
 describe Nmax do
+  before (:all) do
+    @test_file = File.open('spec/test_text.txt', 'r')
+  end
+
+
   it 'has a version number' do
     expect(Nmax::VERSION).not_to be nil
   end
 
-  # it 'does something useful' do
-  #   expect(false).to eq(true)
-  # end
+  it 'should return correct answer' do
+    test_array = Nmax.get_sequences("123qwerty432 101", 3)
+    expect(test_array).to eq([432, 123, 101])
+  end
+
+  it "should return correct answer from file" do
+    test_array = Nmax.get_sequences(@test_file, 3)
+    expect(test_array).to eq([101010101012292929295, 101010101012292929293, 232312])
+  end
+
+  it "should return error message if argument is not integer" do
+    test_array = Nmax.get_sequences(@test_file, 'abc')
+    expect(test_array).to eq('Wrong argument - should be integer')
+  end
+
+  after (:all) do
+    @test_file.close
+  end
+
 end
